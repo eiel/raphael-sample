0cb0ed50e61a49d0e8d149db175632f9abbd4df1
Tomohiko Himura <eiel.hal@gmail.com>
2013-09-11 19:34:43 +0900

    STEP3 アニメーションしてみる

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,12 @@
 (function () {
+
 var paper = Raphael(10, 50, 320, 200);
 
-// 円を書く at x = 50, y = 40,  半径 10
 var circle = paper.circle(50, 40, 10);
-// 赤色でぬりつぶす
 circle.attr("fill", "#e00");
-
-// 黒で境界線をかく
 circle.attr("stroke", "#000");
 
-paper = Raphael(10, 240, 320, 200);
+// 2000ms かけて x座標 320 まで移動
+circle.animate({'cx': 320}, 2000);
 
-circle = paper.circle(50, 40, 40);
-circle.attr("fill", "#00e");
-circle.attr("stroke", "#000");
 })();
