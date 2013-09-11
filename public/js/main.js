a3c8324b08a3f983c3791f91632bc74756d40670
Tomohiko Himura <eiel.hal@gmail.com>
2013-09-11 19:26:50 +0900

    STEP2 SVGを複数作成してみる

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,14 @@ var paper = Raphael(10, 50, 320, 200);
 // 円を書く at x = 50, y = 40,  半径 10
 var circle = paper.circle(50, 40, 10);
 // 赤色でぬりつぶす
-circle.attr("fill", "#f00");
+circle.attr("fill", "#e00");
 
 // 黒で境界線をかく
+circle.attr("stroke", "#000");
+
+paper = Raphael(10, 240, 320, 200);
+
+circle = paper.circle(50, 40, 40);
+circle.attr("fill", "#00e");
 circle.attr("stroke", "#000");
 })();
