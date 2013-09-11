9a40dead038e550b456bf8c47613e007861f0866
Tomohiko Himura <eiel.hal@gmail.com>
2013-09-11 19:39:27 +0900

    STEP4 クリックでアニメーションしてみる

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,8 @@ var circle = paper.circle(50, 40, 10);
 circle.attr("fill", "#e00");
 circle.attr("stroke", "#000");
 
-// 2000ms かけて x座標 320 まで移動
-circle.animate({'cx': 320}, 2000);
+circle.click(function () {
+  circle.animate({'cx': 320}, 2000);
+});
 
 })();
